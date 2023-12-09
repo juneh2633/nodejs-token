f6a7bd6984ce8b23caf3e44a7fc5d1acd9533133
Ubuntu <ubuntu@ip-172-31-32-126.ap-northeast-2.compute.internal>
2023-12-09 07:54:38 +0000

    build API bone

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
     // Package
 const express = require("express");
+const url = require("url");
 
     // Init
 const app = express();
@@ -14,6 +15,11 @@ app.get("/loginPage", (req, res) => {
     res.sendFile(`${__dirname}/public/login.html`);
 
 });
+
+// get/accounts 회원정보 목록 가져오기
+// =>get/accounts?id=test1234&password=12341234 로그인 기능
+// =>get/accounts?name=왕준혁&phonenumber=01085490120 아이디 찾기
+// =>get/accounts?id=test1234&name=왕준혁&phonenumber=01085490120 비밀번호 찾기
 app.get("/account", (req, res) => {
     const { idx } = req.body;
     const result = {
@@ -30,15 +36,75 @@ app.get("/account", (req, res) => {
     //값 반환
     res.send(result);
 });
+
+//===============================회원정보========================================
+
+//get/account/1 회원정보 가져오기
+app.get("/account/?", (req, res) => {  
+
+});
+// post/account 회원가입
 app.post("/account", (req, res) => {  
-    const { id, pw, name } = req.body;
+
 });
-app.put("/account", (req, res) => {
+// put/account/1 회원정보 수정
+app.put("/account/?", (req, res) => {
     
 });
-app.delete("/account", (req, res) => {
+// delete/account/1 회원탈퇴
+app.delete("/account/?", (req, res) => {
     
 });
+
+//===============================게시글=====================================
+// get/boards 게시글 목록 가져오기
+app.get("/boards", (req, res) => {  
+
+});
+// get/board/1 게시글 가져오기
+app.get("/board/?", (req, res) => {  
+
+});
+// post/board 게시글 쓰기
+app.post("/board", (req, res) => {  
+
+});
+// put/board/1   게시글 수정
+app.put("/board/?", (req, res) => {
+    
+});
+// delete/board/1 게시글 삭제
+app.delete("/board/?", (req, res) => {
+    
+});
+
+//============================댓글====================================
+
+
+// get/board/1/replies 게시글의 댓글 목록 가져오기
+app.get("/board/?/replies", (req, res) => {  
+
+});
+
+// get/board/1/reply/1 게시글 댓글 가져오기
+app.get("/board/?/reply/?", (req, res) => {  
+
+});
+
+// post/board/1/reply/1 댓글 쓰기
+app.post("/board/?/reply/?", (req, res) => {  
+ 
+});
+// put/board/1/reply/1 댓글 수정
+app.put("/board/?/reply/?", (req, res) => {
+    
+});
+// delete /board/1/reply /1 댓글 삭제
+app.delete("/board/?/reply/?", (req, res) => {
+    
+});
+
+//================================================================================
 
 
 //Web Server
